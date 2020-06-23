21a4d0ada70e9cbb0d69600caaf6d4cd9f4d401f
w1nston <wallander.jonas@gmail.com>
2020-06-24 00:07:13 +0200

    Add floating button for filter options.

--- pages/recipes/index.tsx
+++ pages/recipes/index.tsx
@@ -1,4 +1,6 @@
+/** @jsx jsx */
 import Head from 'next/head';
+import { css, jsx } from '@emotion/core';
 import { getRecipeLinks } from '../../api/recipe';
 import RecipeLinks from '../../components/recipe/RecipeLinks';
 import { IRecipeLink } from '../../api/recipe/index';
@@ -6,6 +8,25 @@ import { IRecipeLink } from '../../api/recipe/index';
 type Props = {
   recipeLinks: IRecipeLink[];
 };
+
+const buttonStyles = css`
+  color: #fefdfa;
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  margin: 0 1rem 1rem 0;
+  width: 4rem;
+  height: 4rem;
+  border-radius: 50%;
+  font-size: 3rem;
+  background-color: tomato;
+  text-decoration: none;
+  border: none;
+`;
+
+function FloatingButton() {
+  return <button css={buttonStyles}>+</button>;
+}
 
 export default function Recipes({ recipeLinks }: Props) {
   return (
@@ -15,6 +36,7 @@ export default function Recipes({ recipeLinks }: Props) {
       </Head>
       <main>
         <RecipeLinks recipeLinks={recipeLinks} />
+        <FloatingButton />
       </main>
     </>
   );
